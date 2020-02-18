5d8282e3e77a8994d4a2dd8bdf79a6ca2699287d
Sidiq Aldi <sidiq.aldi@gmail.com>
2020-02-18 23:37:56 +0700

    remove-preset

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        LaravelFrontendPresets\MaterialPreset\MaterialPresetServiceProvider::class,
     ],
 
     /*
